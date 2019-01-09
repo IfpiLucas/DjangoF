cda309e72d5de0aae3ff9d452fe5301bfcba3034
IfpiLucas <ifpilucas@gmail.com>
2019-01-08 23:37:17 -0300

    Melhorias na Quarta Questão

--- Revista/admin.py
+++ Revista/admin.py
@@ -1,27 +1,3 @@
-# from django.contrib import admin
-#
-# from .models import *
-#
-# class AdminRevista(admin.TabularInline):
-#     model = revista
-#     extra = 1
-#
-# class AdminColeçao(admin.ModelAdmin):
-#     inlines = [AdminRevista]
-#
-# class AdminAmigo(admin.TabularInline):
-#     model = Amigo
-#     extra = 1
-#
-# class AdminEmprestimo(admin.ModelAdmin):
-#     inlines = [AdminAmigo]
-#
-# admin.site.register(revista)
-# admin.site.register(caixa)
-# admin.site.register(coleçao, AdminColeçao)
-# admin.site.register(emprestimo, AdminEmprestimo)
-# admin.site.register(Amigo)
-#
 from django.contrib import admin
 
 from .models import *
--- Revista/models.py
+++ Revista/models.py
@@ -1,38 +1,3 @@
-# from django.db import models
-#
-# class coleçao(models.Model):
-#     nome = models.CharField(max_length=20)
-#
-#
-# class caixa(models.Model):
-#     numero = models.IntegerField()
-#     etiqueta = models.CharField(max_length=20)
-#     cor = models.CharField(max_length=15)
-#
-#     def __str__(self):
-#         return self.etiqueta
-#
-# class revista(models.Model):
-#     numero_ediçao = models.IntegerField()
-#     ano = models.IntegerField()
-#     coleçao = models.ForeignKey(coleçao, on_delete=models.CASCADE)
-#     caixa = models.ForeignKey(caixa, on_delete=models.CASCADE)
-#
-#
-# grupo = (('1','Predio'),('2','Escola'))
-# class Amigo(models.Model):
-#     nome = models.CharField(max_length=100)
-#     nome_da_mae = models.CharField(max_length=100)
-#     telefone = models.CharField(max_length=15)
-#     grupo_amigo = models.CharField(max_length=100, choices=grupo)
-#
-#
-# class emprestimo(models.Model):
-#     amigo = models.ForeignKey(Amigo, on_delete=models.CASCADE)
-#     magazine = models.ForeignKey(revista, on_delete=models.CASCADE)
-#     data_emprestimo = models.DateField()
-#     data_devoluçao = models.DateField()
-#
 from django.db import models
 
 grupo = (('E', 'Escola'),('P','Prédio'))
@@ -49,6 +14,8 @@ class Caixa(models.Model):
     etiqueta = models.CharField(max_length=100)
     cor = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.etiqueta
 
 class Revista(models.Model):
     numero_edicao = models.IntegerField()
@@ -72,4 +39,3 @@ class Emprestimo(models.Model):
     amigo = models.ForeignKey(Amigo, on_delete=models.CASCADE)
     data_emprestimo = models.DateField(auto_now_add=True)
     data_devolucao = models.DateField()
-#
